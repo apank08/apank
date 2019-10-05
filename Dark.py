eaab53021968c44e9b5de39a14e5955e83ed40ad
apank08 <56200061+apank08@users.noreply.github.com>
2019-10-05 23:35:49 +0700

    Create Dark.py

---
+++ Dark.py
@@ -0,0 +1,15 @@
+apt update && apt upgrade
+
+pkg install git 
+
+pkg install python2
+
+git clone https://github.com/apank08/darkfb
+
+pip2 install requests
+
+pip2 install mechanize
+
+cd darkfb
+
+python2 Dark.py
